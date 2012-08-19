8c89d327de7d728fc9a115c00559db7092deb8ef
Miklos N. Szabo <software@synthoid.hu>
2012-08-20 00:47:09 +0200

    Added licence information

--- pyside/base.py
+++ pyside/base.py
@@ -1,20 +1,46 @@
 #!/usr/bin/python2
 # -*- charset: utf-8 -*-
+
 #
-#    PySide template
+# Name:    PySide base template
+# Licence: MIT   
+#
+# Copyright (C) 2012 Miklos Nemeth Szabo, Synthoid Software
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy 
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in 
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 #
 
-import sys
 from PySide.QtCore import *
 from PySide.QtGui import *
+import sys
 
-
+# Main window class
 class MainWindow(QMainWindow):
-    def __init__(self,parent=None):
+    def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
-        self.resize(800,600)
+        
+        # Resize the window
+        self.resize(800, 600)
+        
+        
 
-
+# c like main()
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     win = MainWindow()
