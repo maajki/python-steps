160c7e728ead1d0f36bbf1517499d89e48d74dab
Miklós Németh Szabó <miklos.nszabo@gmail.com>
2013-09-14 02:26:11 +0200

    Revert "changed copyright informations"
    This reverts commit 41ce1ded3d034aad2a5a08c06a9a5f909c1542b5.

--- pyside/menu.py
+++ pyside/menu.py
@@ -5,7 +5,7 @@
 # Name:    PySide menu template
 # Licence: MIT   
 #
-# Copyright (C) 20122013 Miklos Nemeth Szabo, Maajki Software
+# Copyright (C) 2012 Miklos Nemeth Szabo, Synthoid Software
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy 
 # of this software and associated documentation files (the "Software"), to deal
